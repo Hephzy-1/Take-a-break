3598db4bb1dccfee4876ecfffa1caa0bd5773b01
Hephzy-1 <hzdelight01@gmail.com>
2024-07-03 22:21:02 +0100

    contact us

--- controllers/contact.js
+++ controllers/contact.js
@@ -2,7 +2,7 @@ const { contactUs } = require('../utils/email');
 
 async function contact (req, res) {
   try {
-    const { fullName, email, subject, message } = req.body;
+    const { email, fullName, subject, message } = req.body;
 
     const sent = await contactUs(email, subject, fullName, message);
     console.log(sent);
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const route = express.Router();
+const { body } = require('express-validator')
 const { contact } = require('../controllers/contact');
 
-route.post('/message', contact);
+route.post('/message', [
+  body('email').isEmail().withMessage('Please enter a valid email'),
+  body('fullName').isAlpha().withMessage('Please input your full name'),
+  body('subject').isAlpha().withMessage('Please enter a contact subject'),
+  body('message').exists().withMessage('Contact has to include a message')
+], contact);
 
 module.exports = route;
