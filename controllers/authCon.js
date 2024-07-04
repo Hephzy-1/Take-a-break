8ca79f2c42147c41a8775a2183dec7ae92d09f47
Hephzy-1 <hzdelight01@gmail.com>
2024-07-04 16:48:53 +0100

    what happened?

--- controllers/authCon.js
+++ controllers/authCon.js
@@ -55,6 +55,7 @@ async function login (req, res) {
     const { firstName, email, password } = req.body;
 
     const checkUser = await Users.findOne({ firstName: firstName, email });
+    console.log(checkUser);
 
     if(!checkUser) {
       res.status(400).json({ message: 'User not found. Please register first.' })
